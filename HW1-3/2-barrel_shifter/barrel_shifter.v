d63dd400ede6b54d9ddd892587b0c6d683856488
吳書帆 <Frank@wushufande-MacBook-Pro.local>
2018-03-19 20:02:42 +0800

    0

--- HW1-3/2-barrel_shifter/barrel_shifter.v
+++ HW1-3/2-barrel_shifter/barrel_shifter.v
@@ -4,6 +4,18 @@ input  [2:0] shift;
 output [7:0] out;
 
 /*Write your code here*/
+wire [7:0] x1;
+wire [7:0] x2;
+
+assign x1[7:1]=(shift[0]==1)?in[6:0]:in[7:1];
+assign x1[0]=(shift[0]==1)?0:in[0];
+
+assign x2[7:2]=(shift[1]==1)?in[5:0]:x1[7:2];
+assign x2[1:0]=(shift[1]==1)?0:x1[1:0];
+
+assign out[7:4]=(shift[2]==1)?in[3:0]:x2[7:4];
+assign out[3:0]=(shift[2]==1)?0:x2[3:0];
+
 
 
 /*End of code*/
