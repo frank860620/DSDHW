83c9b82b6bfc7b504c7ccaf6603b3de5823728d8
吳書帆 <Frank@wushufande-MacBook-Pro.local>
2018-03-29 03:03:26 +0800

    3

--- HW1-3/3-ASU/adder_gate.v
+++ HW1-3/3-ASU/adder_gate.v
@@ -2,12 +2,12 @@ module pfa(a,b,p,g);  //A one PFA. I need 16 of them5
     //wire w;
     //reg a,b,c;
     //wire sum,p,g;
-    input a,b;
-    output p,g;
+    input #1 a,b;
+    output #1 p,g;
 
     //xor (w,a,b);     //repeated P. May need it may not. 
-    and (g,a,b);  //Gi
-    xor (p,a,b);   //Pi
+    and #1 (g,a,b);  //Gi
+    xor #1 (p,a,b);   //Pi
     //xor (sum,w,c);  //sum 
 endmodule
                 //input    output   
@@ -24,30 +24,30 @@ module half_adder_gate(A,B,Cin,carry,out);
     wire c1,c2,c3,c4,c5,c6,c7;
 
         pfa PFA0(A[0],B[0],P[0],G[0]);
-        and (w3,P[0],Cin);
-        or (c1,G[0],w3);
+        and #1 (w3,P[0],Cin);
+        or #1 (c1,G[0],w3);
         pfa PFA1(A[1],B[1],P[1],G[1]);
-        and (w4,P[1],G[0]);
-        and (w5,P[1],P[0],Cin);
-        or (c2,G[1],w4,w5); 
+        and #1 (w4,P[1],G[0]);
+        and #1 (w5,P[1],P[0],Cin);
+        or #1 (c2,G[1],w4,w5); 
         pfa PFA2(A[2],B[2],P[2],G[2]);
-        and (w6,P[2],G[1]);
-        and (w7,P[2],P[1],G[0]);
-        and (w8,P[2],P[1],P[0],Cin);
-        or (c3,G[2],w6,w7,w8);
+        and #1 (w6,P[2],G[1]);
+        and #1 (w7,P[2],P[1],G[0]);
+        and #1 (w8,P[2],P[1],P[0],Cin);
+        or #1 (c3,G[2],w6,w7,w8);
         pfa PFA3(A[3],B[3],P[3],G[3]);
-        and (w9,P[3],G[2]);
-        and (w10,P[3],P[2],G[1]);
-        and (w11,P[3],P[2],P[1],G[0]);
-        and (w12,P[3],P[2],P[1],P[0],Cin);
-        or(carry,G[3],w9,w10,w11,w12);
+        and #1 (w9,P[3],G[2]);
+        and #1 (w10,P[3],P[2],G[1]);
+        and #1 (w11,P[3],P[2],P[1],G[0]);
+        and #1 (w12,P[3],P[2],P[1],P[0],Cin);
+        or #1 (carry,G[3],w9,w10,w11,w12);
         
   
 
-        xor (out[0],P[0],Cin);
-        xor (out[1],P[1],c1);
-        xor (out[2],P[2],c2);
-        xor (out[3],P[3],c3);
+        xor #1 (out[0],P[0],Cin);
+        xor #1 (out[1],P[1],c1);
+        xor #1 (out[2],P[2],c2);
+        xor #1 (out[3],P[3],c3);
      
 
 
