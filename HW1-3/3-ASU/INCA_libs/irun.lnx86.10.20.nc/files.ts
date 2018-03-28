650bb2574e9d9100b2fd3398d4d883e72b2a1e8a
frank860620 <b04901099@ntu.edu.tw>
2018-03-28 15:40:08 +0800

    1

---
+++ HW1-3/3-ASU/INCA_libs/irun.lnx86.10.20.nc/files.ts
@@ -0,0 +1,4 @@
+1521346543 /home/raid7_2/userb04/b04099/DSDHW/HW1-3/3-ASU/asu_test.v
+1521476937 /home/raid7_2/userb04/b04099/DSDHW/HW1-3/3-ASU/asu.v
+1521464241 /home/raid7_2/userb04/b04099/DSDHW/HW1-3/3-ASU/adder.v
+1521464241 /home/raid7_2/userb04/b04099/DSDHW/HW1-3/3-ASU/barrel_shifter.v
