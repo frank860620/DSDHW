778efde582ff16ae416a7335a41a2f7dd07e5633
吳書帆 <Frank@alvinx.m6.ntu.edu.tw>
2018-03-28 21:36:24 +0800

    2

--- HW1-3/2-barrel_shifter/barrel_shifter_gate.v
+++ HW1-3/2-barrel_shifter/barrel_shifter_gate.v
@@ -44,10 +44,10 @@ input 	a,b,sel;
 output 	x;
 wire sel_i,w1,w2;
 
-not n0(sel_i,sel);
-and a1(w1,a,sel_i);
-and a2(w2,b,sel);
-or  o1(x,w1,w2);
+not #1 n0(sel_i,sel);
+and #1 a1(w1,a,sel_i);
+and #1 a2(w2,b,sel);
+or  #1 o1(x,w1,w2);
 
 	
 endmodule
