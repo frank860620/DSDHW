04de022051c408b7e58773559e7b0e098dec0fc5
吳書帆 <Frank@alvinx.m6.ntu.edu.tw>
2018-03-28 22:31:15 +0800

    3

--- HW1-3/3-ASU/asu_gate.v
+++ HW1-3/3-ASU/asu_gate.v
@@ -5,7 +5,12 @@ output carry;
 output [7:0] out;
 
 /*Write your code here*/
+wire [7:0] output_a;
+wire [7:0] output_b;
+adder a(x,y,carry,output_a);
+barrel_shifter b(x,y[2:0],output_b);
 
+assign #2.5 out[7:0]=mode?output_a:output_b;
 
 /*End of code*/
 
