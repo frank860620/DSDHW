8a2b76063000c9c918c95293662bf38c0b7a1d68
吳書帆 <Frank@wushufande-MacBook-Pro.local>
2018-04-02 18:23:36 +0800

    hw2

---
+++ DSD_HW2/1-ALU/1_RTL/alu_rtl.v
@@ -0,0 +1,48 @@
+//RTL (use continuous assignment)
+module alu_rtl(
+    ctrl,
+    x,
+    y,
+    carry,
+    out  
+);
+    
+    input  [3:0] ctrl;
+    input  [7:0] x;
+    input  [7:0] y;
+    output       carry;
+    output [7:0] out;
+    wire [7:0] out_0,out_1,out_2,out_3,out_4,out_5;
+    wire [7:0] out_6,out_7,out_8,out_9,out_10,out_11;
+    wire carry_0,carry_1;
+    wire zero=0;
+    assign {carry_0,out_0}=x+y;
+    assign {carry_1,out_1}=x-y;
+    assign out_2=x&y;
+    assign out_3=x|y;
+    assign out_4=~x;
+    assign out_5=x^y;
+    assign out_6=~(x|y);
+    assign out_7=y << x[2:0];
+    assign out_8=y >> x[2:0];
+    assign out_9={x[7],x[7:1]};
+    assign out_10={x[0],x[7:1]};
+    assign out_11=(x==y)?1:0;
+    assign {carry,out[7:0]}=(ctrl==4'b0000)?{carry_0,out_0}:
+                            (ctrl==4'b0001)?{carry_1,out_1}:
+                            (ctrl==4'b0010)?out_2:
+                            (ctrl==4'b0011)?out_3:
+                            (ctrl==4'b0100)?out_4:
+                            (ctrl==4'b0101)?out_5:
+                            (ctrl==4'b0110)?out_6:
+                            (ctrl==4'b0111)?out_7:
+                            (ctrl==4'b1000)?out_8:
+                            (ctrl==4'b1001)?out_9:
+                            (ctrl==4'b1010)?out_10:
+                            (ctrl==4'b1011)?out_11:zero;
+
+
+
+         
+
+endmodule
---
+++ HW1-3/DSD_HW1_b04901099/2-barrel_shifter/barrel_shifter.v
@@ -0,0 +1,18 @@
+module barrel_shifter(in, shift, out);
+input  [7:0] in;
+input  [2:0] shift;
+output [7:0] out;
+
+/*Write your code here*/
+wire [7:0] x1;
+wire [7:0] x2;
+reg zero=0;
+
+assign x1[7:0]=shift[0]?{in[6:0],zero}:in[7:0];
+assign x2[7:0]=shift[1]?{x1[5:0],zero,zero}:x1[7:0];
+assign out[7:0]=shift[2]?{x2[3:0],zero,zero,zero,zero}:x2[7:0];
+
+
+
+/*End of code*/
+endmodule
