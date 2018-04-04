5797732f4231c232415714d8b284668ca39b6db5
frank860620 <b04901099@ntu.edu.tw>
2018-04-04 16:11:28 +0800

    2

--- DSD_HW2/2-RegisterFile/register_file.v
+++ DSD_HW2/2-RegisterFile/register_file.v
@@ -10,20 +10,54 @@ module register_file(
 );
 input        Clk, WEN;
 input  [2:0] RW, RX, RY;
-input  [7:0] busW;
-output [7:0] busX, busY;
+input  reg [7:0] busW;
+output reg [7:0] busX, busY;
     
 // write your design here
 reg [7:0] r0_w, r1_w, r2_w, r3_w, r4_w, r5_w, r6_w, r7_w;
 reg [7:0] r0_r, r1_r, r2_r, r3_r, r4_r, r5_r, r6_r, r7_r;
+r0_w=0;
+r0_r=0;    
+always @(*) begin
+    case (RX)
+	0: busX = r0_r;
+	1: busX = r1_r;
+	2: busX = r2_r;
+	3: busX = r3_r;
+    4: busX = r4_r;
+    5: busX = r5_r;
+    6: busX = r6_r;
+    7: busX = r7_r;
+	default: busX = 16'hXXXX;
+    endcase
+   end
 
-    
-always@(*) begin
+always @(*) begin
+    case (RY)
+	0: busY = r0_r;
+	1: busY = r1_r;
+	2: busY = r2_r;
+	3: busY = r3_r;
+    4: busY = r4_r;
+    5: busY = r5_r;
+    6: busY = r6_r;
+    7: busY = r7_r;
+	default: busY = 16'hXXXX;
+    endcase
+   end
 
-end
-
-always@(posedge Clk) begin
-
-end	
+always @(posedge clk) begin
+    if (WEN) 
+	case (RW) 
+	  0: r0_w <= busW;
+	  1: r1_w <= busW;
+	  2: r2_w <= busW;
+	  3: r3_w <= busW;
+      4: r4_w <= busW;
+      5: r5_w <= busW;
+      6: r6_w <= busW;
+      7: r7_w <= busW;
+	endcase 
+   end 
 
 endmodule
--- DSD_HW2/2-RegisterFile/register_file_tb.v
+++ DSD_HW2/2-RegisterFile/register_file_tb.v
@@ -8,6 +8,7 @@ module register_file_tb;
     reg  [2:0] RW, RX, RY;
     reg  [7:0] busW;
     wire [7:0] busX, busY;
+    reg [7:0] regX, regY;
     
     // instantiate the design-under-test
     register_file rf(
@@ -22,6 +23,32 @@ module register_file_tb;
     );
 
     // write your test pattern here
-
-
+initial begin
+    busW=8'd0;
+    busX=8'd0;
+    busY=8'd0;
+    RX=3'd0;
+    RY=3'd0;
+    RW=3'd0;
+    WEN=0;
+    Clk=0;
+    regX=busX;
+    regY=busY;
+  end
+always begin #(`CYCLE * 0.5) Clk = ~Clk;
+end
+always @(posedge Clk) begin
+ 
+ // Test Case 1: 
+  //   Write '42' to register 2, verify with Read Ports 1 and 2
+  //   (Passes because example register file is hardwired to return 42)
+  RW = 3'd2;
+  busW = 8'd42;
+  WEN= 1;
+  regX = 3'd2;
+  regY = 3'd2;
+  //#(`HCYCLE) Clk=1; #5 Clk=0;	// Generate single clock pulse
+  if((regX != 42) || (regY != 42)) begin
+  $display("Test Case 1 Failed");
+  end
 endmodule
