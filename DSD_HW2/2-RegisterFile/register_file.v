1a7061068177f8c1f77d3a36d7e02ebdcc172e36
frank860620 <b04901099@ntu.edu.tw>
2018-04-04 16:18:22 +0800

    2

--- DSD_HW2/2-RegisterFile/register_file.v
+++ DSD_HW2/2-RegisterFile/register_file.v
@@ -14,10 +14,9 @@ input  [7:0] busW;
 output reg [7:0] busX, busY;
     
 // write your design here
-reg [7:0] r0_w, r1_w, r2_w, r3_w, r4_w, r5_w, r6_w, r7_w;
-reg [7:0] r0_r, r1_r, r2_r, r3_r, r4_r, r5_r, r6_r, r7_r;
-r0_w=0;
-r0_r=0;    
+reg [7:0] r0_w=0, r1_w, r2_w, r3_w, r4_w, r5_w, r6_w, r7_w;
+reg [7:0] r0_r=0, r1_r, r2_r, r3_r, r4_r, r5_r, r6_r, r7_r;
+   
 always @(*) begin
     case (RX)
 	0: busX = r0_r;
