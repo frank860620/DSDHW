3a1394920e0498d857aa86a018d056a992266b22
frank860620 <b04901099@ntu.edu.tw>
2018-04-04 17:10:06 +0800

    2

--- DSD_HW2/2-RegisterFile/register_file_tb.v
+++ DSD_HW2/2-RegisterFile/register_file_tb.v
@@ -48,8 +48,8 @@ always @(posedge Clk) begin
   $display("Test Case 1 Begin");
   $display("busX=%d,busY=%d",busX,busY);
   
-  #(`HCYCLE) Clk=1; #(`HCYCLE) Clk=0;	// Generate single clock pulse
-
+  //#(`HCYCLE) Clk=1; #(`HCYCLE) Clk=0;	// Generate single clock pulse
+ $display(Clk);
   if((busX != 42) || (busY != 42)) begin
   $display("Test Case 1 Failed");
   $display("regX=%d,regy=%d",regX,regY);
