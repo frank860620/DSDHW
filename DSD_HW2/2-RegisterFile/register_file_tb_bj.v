f3f14260dfa7d08a5aa7b9f897ec39e64230d2ff
frank860620 <b04901099@ntu.edu.tw>
2018-04-04 18:51:30 +0800

    2

--- DSD_HW2/2-RegisterFile/register_file_tb_bj.v
+++ DSD_HW2/2-RegisterFile/register_file_tb_bj.v
@@ -1,20 +1,21 @@
 `timescale 1ns/10ps
-`define CYCLE  10
+`define CYCLE  20
 `define HCYCLE  5
 
 module register_file_tb;
     // port declaration for design-under-test
-    parameter pattern_num = 10;
+    parameter pattern_num = 100;
+
     reg Clk, WEN;
     reg  [2:0] RW, RX, RY;
     reg  [7:0] busW;
     wire [7:0] busX, busY;
     
     reg stop;
-    reg [2:0] register_number;
-    reg [7:0] register_value;
-    integer i,num,error;
-    
+    reg [2:0] register_to_write;
+    reg [7:0] register_content;
+    integer error, i, num;
+
     // instantiate the design-under-test
     register_file rf(
         Clk  ,
@@ -26,9 +27,8 @@ module register_file_tb;
         busX ,
         busY
     );
-   
-    // write your test pattern here
-    initial begin
+
+    initial begin 
         Clk = 1'b1;
         error = 0;
         stop = 0;
@@ -41,41 +41,41 @@ module register_file_tb;
     end
 
     initial begin
-        for( num = 0 ; num < (pattern_num) ; num = num + 1 )begin
-            @(posedge Clk)begin
-            register_number[2:0] = {$random}%8;
-            register_value[7:0] = {$random}%256;
-            RX = register_number;
-            RY = register_number;
-            RW = register_number;
-            busW = register_value; 
+        for(num = 0; num < (pattern_num); num = num + 1) begin
+            @(posedge Clk) begin
+                register_to_write[2:0] = {$random} % 8;  //generate a random number: 0 ~ 7
+                register_content[7:0] = {$random} % 256; //generate a random number: 0 ~ 255
+                RX[2:0] = register_to_write;
+                RY[2:0] = register_to_write;
+                RW[2:0] = register_to_write;
+                busW[7:0] = register_content;
             end
         end
-    end 
+    end
 
     always@(posedge Clk) begin
         i <= i + 1;
         if (i >= pattern_num)
             stop <= 1;
-        end
-    
-    always@(posedge Clk) begin
-        if((busX !== register_value) || (busY !== register_value)) begin
-            if (register_number !== 0) begin
+    end
+
+    always@(posedge Clk ) begin
+        if((busX !== register_content) || (busY !== register_content)) begin
+            if (register_to_write !== 0) begin
                 error <= error + 1;
                 $display(busX);
                 $display(busY);
-                $display("An ERROR occurs at case %d : {register, register_value} =  {%b , %b}.\n", i, register_number, register_value);
+                $display("An ERROR occurs at no. %d pattern: {register, register_content} =  {%b , %b}.\n", i, register_to_write, register_content);
             end
             else begin
                 if((busX !== 0) || (busY !== 0)) begin
                     error <= error + 1;
-                    $display("An ERROR occurs at case %d: Register[0] = %b", i, register_value);
+                    $display("An ERROR occurs at no. %d pattern: Register[0] not equal to 0, but %b", i, register_content);
                 end
             end
         end
     end
-    
+
     initial begin
         @(posedge stop) begin
             if(error == 0) begin
@@ -91,5 +91,5 @@ module register_file_tb;
             $finish;
         end
     end
-
+   
 endmodule
