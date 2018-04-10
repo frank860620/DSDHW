636e6a801f759caa87f27ddd4c3c0eb7e361e428
frank860620 <b04901099@ntu.edu.tw>
2018-04-10 20:51:25 +0800

    3

--- DSD_HW2/3-SimpleCalculator/simple_calculator.v
+++ DSD_HW2/3-SimpleCalculator/simple_calculator.v
@@ -53,8 +53,8 @@ module mux(Sel,a,b,out);
     reg out;
     always@(a or b or sel)begin
       case(sel)
-      1b'1:out=a;
-      1b'0:out=b;
+      1'b1:out=a;
+      1'b0:out=b;
       endcase
     end
 endmodule
