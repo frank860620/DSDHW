b593cf714f138428ce1b94a615d9b0eec97c4d57
frank860620 <b04901099@ntu.edu.tw>
2018-04-12 00:11:31 +0800

    1

--- DSD_HW2/1-ALU/2_Behavior/alu_tb2.v
+++ DSD_HW2/1-ALU/2_Behavior/alu_tb2.v
@@ -9,7 +9,8 @@ module alu_tb;
     reg  [7:0] y;
     wire       carry;
     wire [7:0] out;
-    
+    integer error;
+
     alu alu1(
         ctrl     ,
         x        ,
@@ -18,74 +19,109 @@ module alu_tb;
         out  
     );
 
-    initial begin
+    /*initial begin
         $fsdbDumpfile("alu.fsdb");
         $fsdbDumpvars;
-    end
+    end*/
 
     initial begin
         ctrl = 4'b1101;
         x    = 8'b00000001;
         y    = 8'b11111111;
-        
-       
-        
-        // 0100 boolean not
-        
-
+        error = 0;
         ctrl = 4'b0000;
+        
+        //start to test
         #2
         if( out == 8'b0000_0000 && carry==1 ) $display( "PASS --- 0000 boolean add" );
-        else $display( "FAIL --- 0000 boolean add" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0000 boolean add" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0000_0010 && carry==1 ) $display( "PASS --- 0001 boolean sub" );
-        else $display( "FAIL --- 0001 boolean sub" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0001 boolean sub" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0000_0001) $display( "PASS --- 0010 boolean and" );
-        else $display( "FAIL --- 0010 boolean and" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0010 boolean and" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b1111_1111) $display( "PASS --- 0011 boolean or" );
-        else $display( "FAIL --- 0011 boolean or" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0011 boolean or" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b1111_1110) $display( "PASS --- 0100 boolean not" );
-        else $display( "FAIL --- 0100 boolean not" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0100 boolean not" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b1111_1110) $display( "PASS --- 0101 boolean xor" );
-        else $display( "FAIL --- 0101 boolean xor" );
+        else begin 
+            error <= error + 1;
+            $display( "FAIL --- 0101 boolean xor" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0000_0000) $display( "PASS --- 0110 boolean nor" );
-        else $display( "FAIL --- 0110 boolean nor" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0110 boolean nor" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b1111_1110) $display( "PASS --- 0111 boolean Shift left logical variable" );
-        else $display( "FAIL --- 0111 boolean Shift left logical variable" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 0111 boolean Shift left logical variable" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0111_1111) $display( "PASS --- 1000 boolean Shift right logical variable" );
-        else $display( "FAIL --- 1000 boolean Shift right logical variable" );
-        //$display("ctrl=%b,x=%b,y=%b,carry=%b,out=%b",ctrl,x,y,carry,out);  
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 1000 boolean Shift right logical variable" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0000_0000) $display( "PASS --- 1001 boolean Shift right arithmetic" );
-        else $display( "FAIL --- 1001 boolean Shift right arithmetic" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 1001 boolean Shift right arithmetic" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0000_0010) $display( "PASS --- 1010 boolean Rotate left" );
-        else $display( "FAIL --- 1010 boolean Rotate left" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 1010 boolean Rotate left" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b1000_0000) $display( "PASS --- 1011 boolean Rotate right" );
-        else $display( "FAIL --- 1011 boolean Rotate right" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 1011 boolean Rotate right" );
+        end
         ctrl = ctrl + 1;
         #2
         if( out == 8'b0000_0000) $display( "PASS --- 1100 boolean Equal" );
-        else $display( "FAIL --- 1100 boolean Equal" );
+        else begin
+            error <= error + 1;
+            $display( "FAIL --- 1100 boolean Equal" );
+        end
         ctrl = ctrl + 1;
         #2
         ctrl = ctrl + 1;
@@ -93,12 +129,24 @@ module alu_tb;
         ctrl = ctrl + 1;
         #2
         ctrl = ctrl + 1;
-
-
-        // finish tb
-        #(`CYCLE) $finish;
         
+        
+        //count the error
+        if(error == 0) begin
+            $display("==========================================\n");
+            $display("======  Congratulation! You Pass!  =======\n");
+            $display("==========================================\n");
+        end
+        else begin
+            $display("===============================\n");
+            $display("There are %d errors.", error);
+            $display("===============================\n");
+        end
+    $finish;
     end
+    endmodule
+
+    
     /*
     always #2 begin
     $display("ctrl=%b,x=%b,y=%b,carry=%b,out=%b",ctrl,x,y,carry,out);     
@@ -108,4 +156,4 @@ module alu_tb;
     initial #(`CYCLE) $finish;
     */
 
-endmodule
+
