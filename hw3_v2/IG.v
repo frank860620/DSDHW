31590bd7b6abdbcfb6c32aaec3097c0accb64615
frank860620 <b04901099@ntu.edu.tw>
2018-04-26 19:34:21 +0800

    i

--- hw3_v2/IG.v
+++ hw3_v2/IG.v
@@ -20,8 +20,8 @@ reg[7:0] counter,send;
 integer i;
 reg img_rd,grad_wr;
 reg [15:0] img_addr, grad_addr;
-reg[7:0] img_di;
-reg[19:0] grad_do;
+reg [7:0] img_di;
+reg [19:0] grad_do;
 //wire [7:0] in;
 //------------------------------------------------------------------
 // combinational part
