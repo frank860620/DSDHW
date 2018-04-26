eb9d1b08ebdcc5c465030cfdc98d4548de07bde7
frank860620 <b04901099@ntu.edu.tw>
2018-04-26 12:43:24 +0800

    i

--- hw3_v2/IG.v
+++ hw3_v2/IG.v
@@ -27,17 +27,17 @@ assign send=0;
 //assign in = img_di;
 for (i=0;i<=65279;i=i+1) begin
     if(counter != 256) begin
-       Gx = rd_M[i+1] - rd_M[i];
-       Gy = rd_M[i+256] - rd_M[i];
-       grad_M[i] = {Gx,Gy};
-       counter = counter + 1; 
+       assign Gx = rd_M[i+1] - rd_M[i];
+       assign Gy = rd_M[i+256] - rd_M[i];
+       assign grad_M[i] = {Gx,Gy};
+       assign counter = counter + 1; 
     end
     else if (counter == 256 && i == 65279)begin
-        counter = 1;
-        send = 1;
+        assign counter = 1;
+        assign send = 1;
     end
     else begin 
-        counter = 1;
+        assign counter = 1;
     end
 end
 
