75efa9188515d695beaf4dd6fe4450d815f38da9
frank860620 <b04901099@ntu.edu.tw>
2018-04-27 01:13:40 +0800

    i

--- hw3_v2/IG.v
+++ hw3_v2/IG.v
@@ -88,9 +88,9 @@ always @(posedge clk) begin
         //rd_M[addr] <= img_di_reg;
         //$display("addr : %d, rd_M[addr] : %d",addr,rd_M[addr]);
         $display("img_addr :",img_addr);
-        if(img_addr == 0 && delay == 0) begin
+        if(img_addr == 0 && delay != 2) begin
         img_addr <= img_addr;
-        delay = 1;
+        delay = dealy + 1;
         $display("b");
         end
         else img_addr <= img_addr+1;
