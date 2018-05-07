c4619118c6e183c3faf30559d74d0d55b4691c03
frank860620 <b04901099@ntu.edu.tw>
2018-05-07 16:20:37 +0800

    3

--- DSD_HW3/verilog/DSDHW3.v
+++ DSD_HW3/verilog/DSDHW3.v
@@ -53,16 +53,16 @@ module SingleCycle_MIPS(
     output        OEN;  // output_enable, 0
 
 //==== reg/wire declaration ===============================
-/*reg [25:0] Inst_25_0;
+reg [25:0] Inst_25_0;
 reg [4:0] Inst_25_21;
 reg [4:0] Inst_20_16;
 reg [4:0] Inst_15_11;
-reg [15:0] Inst_15_0;*/
-wire [25:0] Inst_25_0;
+reg [15:0] Inst_15_0;
+/*wire [25:0] Inst_25_0;
 wire [4:0] Inst_25_21;
 wire [4:0] Inst_20_16;
 wire [4:0] Inst_15_11;
-wire [15:0] Inst_15_0;
+wire [15:0] Inst_15_0;*/
 wire [31:0] shamt; 
 wire [31:0] pc; 
 wire [5:0] opcode;
@@ -96,11 +96,11 @@ wire [31:0] br_signext_sl2;
 
 assign opcode = IR[31:26];
 assign funct  = IR[5:0];
-assign Inst_25_0   = IR[25:0];
+/*assign Inst_25_0   = IR[25:0];
 assign Inst_25_21  = IR[25:21];
 assign Inst_20_16  = IR[20:16];
 assign Inst_15_11  = IR[15:11];
-assign Inst_15_0   = IR[15:0];
+assign Inst_15_0   = IR[15:0];*/
 assign shamt       = {27'd0,IR[10:6]};
 assign OEN = 0;
 assign WEN = !MemWrite;
@@ -196,14 +196,14 @@ always@(IR)begin
 $display("IR=%h",IR);
 $display("Inst_25_21=%d",Inst_25_21);
 end
-/*
+
 always@(IR)begin
 Inst_25_0   = IR[25:0];
 Inst_25_21  = IR[25:21];
 Inst_20_16  = IR[20:16];
 Inst_15_11  = IR[15:11];
 Inst_15_0   = IR[15:0];
-end*/
+end
 /*always@(A)begin
 $display("A=%d",A);
 $display("WEN=%d",WEN);
