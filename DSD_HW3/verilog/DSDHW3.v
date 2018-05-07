22f2678f7662cf5157f2f1ebe08316cb84cccad3
frank860620 <b04901099@ntu.edu.tw>
2018-05-07 20:16:25 +0800

    3

--- DSD_HW3/verilog/DSDHW3.v
+++ DSD_HW3/verilog/DSDHW3.v
@@ -423,6 +423,7 @@ end
 assign ReadData1 = (Reg_R1==5'd0) ? 32'd0 : register[Reg_R1];   
 assign ReadData2 = (Reg_R2==5'd0) ? 32'd0 : register[Reg_R2];
 always@(posedge clk)begin
+$display("RegWrite=%d",RegWrite);
 $display("Reg_R1=%b",Reg_R1);
 $display("Reg_R2=%b",Reg_R2);
 //$display("register[Reg_R2]=%d",register[Reg_R2]);
