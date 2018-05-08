001b7fba450158554db44913361576899a3a1307
frank860620 <b04901099@ntu.edu.tw>
2018-05-08 18:14:48 +0800

    3

--- DSD_HW3/verilog/DSDHW3.v
+++ DSD_HW3/verilog/DSDHW3.v
@@ -361,7 +361,7 @@ output [31:0] ReadData2;
 
 reg[31:0] register[1:31];
 
-always @(negedge rst)begin
+/*always @(negedge rst)begin
 //register[0]=0;
 register[1]=0;
 register[2]=0;
@@ -394,13 +394,45 @@ register[28]=0;
 register[29]=0;
 register[30]=0;
 register[31]=0;
+end*/
+
+
+always@(posedge clk or negedge rst) begin
+if(rst = 1'b0)begin
+register[1]=0;
+register[2]=0;
+register[3]=0;
+register[4]=0;
+register[5]=0;
+register[6]=0;
+register[7]=0;
+register[8]=0;
+register[9]=0;
+register[10]=0;
+register[11]=0;
+register[12]=0;
+register[13]=0;
+register[14]=0;
+register[15]=0;
+register[16]=0;
+register[17]=0;
+register[18]=0;
+register[19]=0;
+register[20]=0;
+register[21]=0;
+register[22]=0;
+register[23]=0;
+register[24]=0;
+register[25]=0;
+register[26]=0;
+register[27]=0;
+register[28]=0;
+register[29]=0;
+register[30]=0;
+register[31]=0;
 end
-
-
-always@(posedge clk) begin
-  if((RegWrite==1'b1) && (Reg_W!=5'd0)) begin
+else if((RegWrite==1'b1) && (Reg_W!=5'd0)) begin
     register[Reg_W] = WriteData;
-
   end
 end
 
