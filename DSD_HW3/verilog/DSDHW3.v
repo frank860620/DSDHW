644bad426f720960129f83fead30c62b683deda1
frank860620 <b04901099@ntu.edu.tw>
2018-05-09 00:08:05 +0800

    3

--- DSD_HW3/verilog/DSDHW3.v
+++ DSD_HW3/verilog/DSDHW3.v
@@ -317,12 +317,12 @@ output RegWrite;
 output _JAL; 
 output _JR;
 
-`define ADD  6'b100000
-`define SUB  6'b100010
-`define AND  6'b100100 
-`define OR   6'b100101 
+//`define ADD  6'b100000
+//`define SUB  6'b100010
+//`define AND  6'b100100 
+//`define OR   6'b100101 
 `define BEQ  6'b000100
-`define SLT  6'b101010
+//`define SLT  6'b101010
 `define LW   6'b100011
 `define SW   6'b101011
 
